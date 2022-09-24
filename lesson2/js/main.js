f6b0b862429c42698839537f3704db83febd0abf
cvanfleet <cvanfleet@byui.edu>
2022-09-24 00:31:46 -0600

    Finished lesson 2, with some media queries to change on different devices

---
+++ lesson2/js/main.js
@@ -0,0 +1,4 @@
+const today = new Date();
+document.querySelector("footer div span").textContent = today.getFullYear();
+const lastmodified = new Date(document.lastModified);
+document.getElementById("lastupdateddata").textContent = `${lastmodified.getMonth()+1}/${lastmodified.getDate()}/${lastmodified.getFullYear()} ${('0'+lastmodified.getHours()).substr(-2)}:${('0'+lastmodified.getMinutes()).substr(-2)}:${('0' +lastmodified.getSeconds()).substr(-2)}`
